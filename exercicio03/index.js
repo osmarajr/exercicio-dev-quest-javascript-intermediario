799e35d62b0ce0dc1182abf2e954f91b4fc627f3
Osmar Almeida <osmar.ajr10@gmail.com>
2024-06-18 17:10:29 -0300

    add exercicio 03

---
+++ exercicio03/index.js
@@ -0,0 +1,14 @@
+/*
+**Descrição da tarefa**: Inicialmente, temos um quadrado com o fundo preto em que o objetivo desse exercício é que ao clicar nele, seja feita uma verificação se ele possui a classe “azul”. Se ele não possuir, devemos adicionar essa classe usando o método add do classList para adicionar essa classe ao elemento, **lembrando que essa classe deve ser criada previamente no CSS**. Após adicionada a classe, ele ficará com a cor azul, só que ao clicar novamente, deverá retornar a cor original, removendo a classe azul, com a utilização do método **remove.**
+*/
+
+//obter o elemento quadrado
+const quadrado = document.getElementsByClassName('quadrado')
+
+quadrado.addEventListener('click', () => {
+
+    const classeAzulExiste = quadrado.classList.contains('azul');
+
+    console.log(classeAzulExiste);
+
+})
