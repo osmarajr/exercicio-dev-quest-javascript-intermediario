4f1100466e7b2797b58726a5e244919bfb09f936
Osmar Almeida <osmar.ajr10@gmail.com>
2024-06-18 08:40:32 -0300

    finalizado o exercicio 02 js intermediario

---
+++ exercicio01/index.js
@@ -0,0 +1,8 @@
+function trocarCor(){
+    //obter elemento do primeiro quadrado
+    let primeiroQuadrado = document.getElementById('primeiroQuadrado');
+
+    //realizando a mudança de estilo
+    primeiroQuadrado.style.backgroundColor = 'yellow';
+
+}
