0d7451111357712d9e16e5b24211062d329a3ac6
Osmar Almeida <osmar.ajr10@gmail.com>
2024-06-20 10:40:39 -0300

    alterado o codigo para resolucao do ex04 de js intermediario

--- exercicio04/index.js
+++ exercicio04/index.js
@@ -2,12 +2,17 @@
 **Descrição da tarefa:**  Nessa tarefa, você deve criar um ou mais inputs na tela. Ao digitar qualquer texto em um desses inputs, a sua borda deverá ficar com a cor verde. Caso o input estiver vazio, a cor da borda deverá voltar ao normal.
 */
 //obter elemento dos inputs 
-const inputs = document.querySelector('.inputs');
 
-inputs.addEventListener('input', function(){
-    if(inputs.value === ''){
-        inputs.classList.remove('corBordaInput')
-    }else{
-        inputs.classList.add('corBordaInput')
-    }
-})
+//obter o elemento
+const inputText = document.querySelectorAll(".input-text");
+//Percorrer todos elementos
+inputText.forEach(indice=> {
+    //se campo tiver mudança, então realizar os procedimentos
+    indice.addEventListener('change', () => {
+        if(indice !== ""){
+            indice.classList.add("campo-preenchido");
+        }else{
+            indice.classList.remove("campo-preenchido")
+        }
+    });
+});
